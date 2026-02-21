3b97af616329b914771982e8a3705d5267ee3912
Dilan <dilanmd123@gmail.com>
2026-02-21 10:23:46 -0500

    luxe-actualizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    domains: ["img.freepik.com", "media.istockphoto.com", "images.unsplash.com"],
+  },
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'hover'
+  },
   vite: {
     plugins: [tailwindcss()]
   }
